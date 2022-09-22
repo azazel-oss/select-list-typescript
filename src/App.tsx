a903df971a2b6f7fb2d97e690621efd260e5e4e9
Asad Mahmood <asadmhmd1234@gmail.com>
2022-09-22 21:15:13 +0530

    Finish single select logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import { Select } from "./Select";
+import { useState } from "react";
+
+type option = {
+  label: string;
+  value: string;
+};
 
 const options = [
   { label: "First", value: "1" },
@@ -8,9 +14,10 @@ const options = [
   { label: "Fifth", value: "5" },
 ];
 function App() {
+  const [value, setValue] = useState<option | null>(options[0]);
   return (
     <>
-      <Select options={options} />
+      <Select options={options} value={value} onChange={(o) => setValue(o)} />
     </>
   );
 }
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styles from "./select.module.css";
 type SelectOption = {
   label: string;
@@ -5,19 +6,60 @@ type SelectOption = {
 };
 type SelectProps = {
   options: SelectOption[];
-  value?: SelectOption;
-  onChange: (value: SelectOption | undefined) => void;
+  value?: SelectOption | null;
+  onChange: (value: SelectOption | null) => void;
 };
 export function Select({ options, value, onChange }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+  useEffect(() => {
+    setHighlightedIndex(0);
+  }, [isOpen]);
+  function clearOptions() {
+    onChange(null);
+  }
+
+  function selectOption(option: SelectOption) {
+    onChange(option);
+  }
+
+  function isOptionSelected(option: SelectOption) {
+    return option === value;
+  }
+
   return (
-    <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>Value</span>
-      <button className={styles["clear-btn"]}>&times;</button>
+    <div
+      onClick={() => setIsOpen((prevIsOpen) => !prevIsOpen)}
+      onBlur={() => setIsOpen((prevIsOpen) => !prevIsOpen)}
+      tabIndex={0}
+      className={styles.container}
+    >
+      <span className={styles.value}>{value?.label}</span>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          clearOptions();
+        }}
+        className={styles["clear-btn"]}
+      >
+        &times;
+      </button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul className={styles.options}>
-        {options.map((option) => (
-          <li key={option.value} className={styles.option}>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+        {options.map((option, i) => (
+          <li
+            onMouseEnter={() => setHighlightedIndex(i)}
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(option);
+              setIsOpen(false);
+            }}
+            key={option.value}
+            className={`${styles.option}
+            ${isOptionSelected(option) ? styles.selected : ""}
+            ${i === highlightedIndex ? styles.highlighted : ""}`}
+          >
             {option.label}
           </li>
         ))}
